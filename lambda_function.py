ee411fa30c615ee21ec27bcd69302d78dbc42909
Dan Merl <merl@physera.com>
2018-06-19 11:21:16 -0700

    initial commit
    sends SNS notifications about prod, dev, and worker along to workplace

---
+++ lambda_function.py
@@ -0,0 +1,36 @@
+from botocore.vendored import requests
+import os
+
+
+def parse_message(message):
+    foo = [x.split(": ") for x in filter(None, message.split("\n"))]
+    if len(foo) % 2 == 0:
+        return dict(foo)
+    else:
+        return {}
+
+
+def lambda_handler(event, context):
+    headers = {"Authorization": "Bearer {}".format(os.environ['FB_API_TOKEN'])}
+    url = "https://graph.facebook.com/{}/feed".format(
+        os.environ['FB_GROUP_ID']
+    )
+
+    message = event['Records'][0]['Sns']['Message']
+    fields = parse_message(message)
+
+    if 'Application' in fields and 'Environment' in fields and \
+       'Environment URL' in fields and 'Message' in fields:
+        app = fields.get("Application", "App")
+        post = ["# [{} ({})]({})".format(app,
+                                         fields["Environment"],
+                                         fields["Enviornment URL"])]
+        post.append("{}".format(fields["Message"]))
+    else:
+        post = ["# {}".format(message)]
+
+    data = {
+        'formatting': 'MARKDOWN',
+        'message': '\n'.join(post)
+    }
+    requests.post(url, headers=headers, data=data).json()
